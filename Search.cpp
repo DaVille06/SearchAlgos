f6b492f228b7a2d9c488f07286c25ab7b43cf49f
Nick Roberts <roberts.nicholas88@gmail.com>
2023-12-03 22:26:47 -0500

    add in comments about the search types

--- Search.cpp
+++ Search.cpp
@@ -1,9 +1,19 @@
 #include "Search.h"
 
+// Runs in O(n)
+// Checks every item in the array for the numToSearchFor
+// (works on sorted and non-sorted)
+// Returns the position of the item if it is found
+// Returns -1 if it was not found
 void Search::simple_search(int arr[], int numToSearchFor) {
-
+	
 }
 
+// Runs in O(log n)
+// Divides the array in half looking for the numbers to search for
+// (only works on a sorted array)
+// Returns the position of the item if it is found
+// Returns -1 if it was not found
 void Search::binary_search(int arr[], int numToSearchFor) {
 	
 }
