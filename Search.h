c3c473e6bc39d5702f28c843b281da1922ea8773
Nick Roberts <roberts.nicholas88@gmail.com>
2023-12-03 22:18:02 -0500

    init commit - creat search header file

---
+++ Search.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+class Search {
+public:
+	void simple_search(int arr[], int numToSearchFor);
+	void binary_search(int arr[], int numToSearchFor);
+};
+
+#endif // !SEARCH_H
